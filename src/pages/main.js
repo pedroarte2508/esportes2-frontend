46dc40f56b2827d53a1559cf339ea0486aafcb19
pedroarte2508 <pedroarte2508@gmail.com>
2019-04-28 02:56:51 -0300

    Listagem de Equipes

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import api from "../services/api";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
 
 export default class Main extends Component {
   static navigationOptions = {
-    title: "Esportes"
+    title: "Equipes"
+  };
+
+  state = {
+    docs: []
   };
 
   componentDidMount() {
@@ -15,15 +26,79 @@ export default class Main extends Component {
     console.log("iniciando aplicação...");
     const response = await api.get("/teams");
     const { docs } = response.data;
-    console.log(docs);
-    console.log("requisição finalizada...");
+    this.setState({ docs });
   };
+
+  renderItem = ({ item }) => (
+    <View style={styles.teamContainer}>
+      <Text styles={styles.teamTitle}>{item.name}</Text>
+      <Image source={{ uri: item.picture }} style={styles.image} />
+
+      <TouchableOpacity style={styles.teamButton} onPress={() => {}}>
+        <Text style={styles.teamButtonText}>Visualizar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   render() {
     return (
-      <View>
-        <Text>Página Main</Text>
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fafafa",
+    flex: 1
+  },
+
+  teamContainer: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20
+  },
+
+  image: {
+    marginTop: 5,
+    width: 50,
+    height: 50
+  },
+
+  list: {
+    padding: 20
+  },
+
+  teamButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "#00a90a",
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10
+  },
+
+  teamButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#00a90a"
+  },
+
+  teamTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333"
+  }
+});
